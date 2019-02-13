8cd69f1730479e63ee2c5096461f44c987ff1700
cirogoud <45541376+cirogoud@users.noreply.github.com>
2019-02-13 22:23:42 +0100

    Update l3-vector-type.js

--- l3-vector-type.js
+++ l3-vector-type.js
@@ -1,1 +1,25 @@
- 
+function Vec(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vec.prototype.plus = function (vector) {
+    return new Vec(this.x + vector.x, this.y + vector.y);
+};
+
+Vec.prototype.minus = function (vector) {
+    return new Vec(this.x - vector.x, this.y - vector.y);
+};
+
+Object.defineProperty(Vec.prototype, "length", {
+    get: function (vector) {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+});
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5

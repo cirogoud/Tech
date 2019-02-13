4cd29f7bc72c34c6ef71002a47a27a4c638fe3ba
cirogoud <45541376+cirogoud@users.noreply.github.com>
2019-02-13 22:22:26 +0100

    Update l2-bean-counting.js

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,18 @@
- 
+function countChar(someString, someChar) {
+    var count = 0;
+    for (var i = 0; i < someString.length; i++) {
+        if (someString.charAt(i) === someChar) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function countBs(someString) {
+    return countChar(someString, 'B');
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4

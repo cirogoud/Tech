67646764f895a6171bdbdcc930bde5ab431da48c
cirogoud <45541376+cirogoud@users.noreply.github.com>
2019-02-13 22:21:05 +0100

    Update l1-chessboard.js

--- l1-chessboard.js
+++ l1-chessboard.js
@@ -1,1 +1,15 @@
-.
+function chessBoard(size){
+
+  const line = ' #'.repeat( size ),
+        even = line.substring(0,size),
+        odd = line.substring(1,size+1);
+  let   out = '';
+
+  while(size--){
+    out = out + ((size % 2) ? odd: even ) + '\n';
+  }
+
+ return out;
+}
+
+console.log(chessBoard(8));
